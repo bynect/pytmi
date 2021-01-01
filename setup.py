9bbfd6595916d0d458e0bfceb3b2d39c39bc8e6b
bynect <68197565+bynect@users.noreply.github.com>
2021-01-01 04:56:32 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pytmi import __version__ as version
+
+
+description = """TMI (Twitch Messaging Interface) protocol library for Python."""
+
+
+project_url = "https://github.com/bynect/pytmi"
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+
+setup_info = {
+    "name": "pytmi",
+    "version": version,
+    "author": "nect",
+    "description": description,
+   "long_description": readme,
+    "long_description_content_type": "text/markdown",
+    "url": project_url,
+    "packages": find_packages(),
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    "python_requires": '>= 3.6',
+}
+
+
+setup(**setup_info)
